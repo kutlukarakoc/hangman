a207cd575442f72611458c557a4d0f5f43a89165
Kutlu Karakoc <karakockutlu@gmail.com>
2023-02-20 22:41:08 +0300

    added keyboard skeleton

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,5 +1,20 @@
+import { keys } from "../constants/keys";
+import "../styles/keyboard.css";
+
 const Keyboard = () => {
-    return <div>keyboard</div>
+
+    const handleClick = target => {
+        target.classList.add('clicked')
+        target.disabled = true
+    }
+
+    return (
+        <h2 className="keyboard__wrapper">
+            {
+                keys.map(key => <button type="button" key={key} onClick={(e) => handleClick(e.target)}>{key}</button>)
+            }
+        </h2>
+    )
 }
 
 export default Keyboard;
