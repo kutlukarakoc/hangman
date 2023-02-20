6127fc6952ce47fe9e7514bd0504f5ed1b0f1032
Kutlu Karakoc <karakockutlu@gmail.com>
2023-02-20 21:36:39 +0300

    added skeleton of drawing

--- src/components/Drawing.tsx
+++ src/components/Drawing.tsx
@@ -1,5 +1,14 @@
+import '../styles/drawing.css'
+
 const Drawing = () => {
-    return <div>drawing component</div>
+    return (
+        <div className="drawing__wrapper">
+            <div className="drawing__top-line"></div>
+            <div className="drawing__top-hook"></div>
+            <div className="drawing__mid-line"></div>
+            <div className="drawing__bottom-line"></div>
+        </div>
+    )
 }
 
 export default Drawing;
