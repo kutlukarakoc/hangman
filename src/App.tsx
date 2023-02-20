360a09404f5e07ff883054f5be6577e6ae6e2ca1
Kutlu Karakoc <karakockutlu@gmail.com>
2023-02-21 00:00:34 +0300

    added comment lines

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,27 @@ import Word from './components/Word'
 
 function App() {
 
+    // Set the initial state of the word to guess
     const [wordToGuess, setWordToGuess] = useState(() => words[Math.floor(Math.random() * words.length)])
+
+    // Set the initial state of the guessed letters
     const [guessedLetters, setGuessedLetters] = useState<string[]>([])
 
+    // Calculate the incorrect letters that have been guessed
     const incorrectLetters = guessedLetters.filter(letter => wordToGuess.indexOf(letter) < 0)
 
+    // Check if the game has been won or lost
     const isWinner = wordToGuess.split('').every(letter => guessedLetters.indexOf(letter) > -1)
     const isLoser = incorrectLetters.length > 5
 
+    // Function to add a guessed letter to the state
     const addGuessedLetter = useCallback((letter: string) => {
         if(guessedLetters.indexOf(letter) > -1 || isLoser || isWinner) return
 
         setGuessedLetters(currentLetters => [...currentLetters, letter])
     }, [guessedLetters, isWinner, isLoser])
 
+    // Add an event listener to detect keyboard input and update the state accordingly
     useEffect(() => {
         const handler = (e: KeyboardEvent) => {
             const key = e.key
