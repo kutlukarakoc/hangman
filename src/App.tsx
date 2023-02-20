498b495a22c4fbbc2fa8ba9b13b7877fca503376
Kutlu Karakoc <karakockutlu@gmail.com>
2023-02-20 21:00:43 +0300

    set up project and added words json

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+import words from "./words.json"
+
+function App() {
+
+    return (
+        <main className="container">
+            test
+        </main>
+    )
+}
+
+export default App
